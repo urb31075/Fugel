76f9a534d92cbb05dd44bf0b05d101e4a37ea953
Roman <urb31075@mail.ru>
2018-07-26 23:31:45 +0300

    Initial commit

---
+++ main.c
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: urb
+ *
+ * Created on July 26, 2018, 11:30 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) 
+{
+    printf("jhdgjshgdjkhsgfjkhgfdsk");
+    return (EXIT_SUCCESS);
+}
+
